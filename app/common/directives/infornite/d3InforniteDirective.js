6dfe9e8dd2f30eb4eb71ff14f8bd33ba5fd60508
sweety-wo <sweety.webashlar@gmail.com>
2017-03-29 19:26:37 +0530

    Create new node and connect to other tree node.

--- app/common/directives/infornite/d3InforniteDirective.js
+++ app/common/directives/infornite/d3InforniteDirective.js
@@ -18,6 +18,8 @@
                     self.chartId = options && options.chartId ? '#' + options.chartId : 'body';
                     self.parentEle = angular.element(self.chartId).parent();
                     self.chartType = options && options.chartType ? options.chartType : 'force';
+                    self.newNodesArr = [];
+                    self.originalData = [];
 
                     self.treeLayout = d3.layout.tree();
                     self.forceLayout = d3.layout.force().gravity(0.05).charge(-260).linkDistance(60);
@@ -33,12 +35,13 @@
 
                     // initialize svg
                     self.svg = d3.select(self.chartId).append('svg').attr('class', 'd3-infornite-svg');
-                    self.chartWrapper = self.svg.append('g').attr("class", 'infornite-graph-wrapper').call(self.zoom);
+                    self.chartWrapper = self.svg.append('g').attr("class", 'infornite-graph-wrapper').call(self.zoom).on("dblclick.zoom", null);
                     self.rect = self.chartWrapper.append("rect")
                         .style("cursor", "move").style("fill", "none").style("pointer-events", "all");
                     self.chartContainer = self.chartWrapper.append("g").attr("class", 'infornite-graph-container');
                     self.links = self.chartContainer.append("g").attr("class", "links");
                     self.nodes = self.chartContainer.append("g").attr("class", "nodes");
+                    self.newNodes = self.chartContainer.append("g").attr("class", "new-nodes");
                 }
 
                 d3Infornite.prototype.render = function () {
@@ -61,9 +64,14 @@
                 };
 
                 d3Infornite.prototype.update = function (data) {
+                    self.originalData = data;
                     self.render();
                     // Filter node edges.
                     var nodesLen = data.nodes.length;
+                    data.nodes = data.nodes.map(function (d, i) {
+                        d.index = i;
+                        return d;
+                    });
                     data.edges = data.edges.filter(function (d) {
                         return 0 < d.source && d.source < nodesLen && 0 < d.target && d.target < nodesLen;
                     });
@@ -101,14 +109,18 @@
                     self.node = self.nodes.selectAll('.node').data(nodes);
 
                     // enter selection
-                    self.nodeEnter = self.node.enter().append('g').attr('class', 'node');
+                    self.nodeEnter = self.node.enter().append('g')
+                        .attr('id', function (d) {
+                            return d.data.index;
+                        })
+                        .attr('class', 'node');
                     self.nodeEnter.append('circle');
                     self.nodeEnter.append('text');
 
                     // update selection
                     self.node.attr('class', 'node')
                         .style("cursor", "pointer")
-                        .on("click", fnToggleNode)
+                        .on("dblclick", fnToggleNode)
                         .call(self.drag);
 
                     self.node.select('circle')
@@ -130,6 +142,77 @@
                     /*----- END : Create nodes -----*/
 
                     self.visualization();
+                };
+
+                d3Infornite.prototype.addNewNode = function (newNodeData) {
+                    newNodeData.index = self.originalData.nodes.length;
+                    self.originalData.nodes.push(newNodeData);
+                    self.newNodesArr.push(newNodeData);
+                    var node = self.newNodes.selectAll('.new-node').data(self.newNodesArr);
+
+                    // enter selection
+                    var nodeEnter = node.enter().append('g');
+                    nodeEnter.append('line');
+                    nodeEnter.append('circle');
+                    nodeEnter.append('text').attr("class", "node-center");
+                    nodeEnter.append('text').attr("class", "node-label");
+                    nodeEnter.append('foreignObject');
+
+                    // update selection
+                    node.attr('class', 'new-node')
+                        .attr('id', function (d) {
+                            return d.index;
+                        })
+                        .attr('transform', function (d, i) {
+                            return 'translate(40,' + ((i + 1) * 40) + ')';
+                        })
+                        .style("cursor", "pointer")
+                        .on("dblclick", function () {
+                            if (d3.select(this).attr('draw-line')) {
+                                d3.select(this).attr('draw-line', null);
+                            } else {
+                                d3.select(this).attr('draw-line', 'true');
+                            }
+                        })
+                        .call(self.drag);
+
+                    node.select("line")
+                        .style("stroke", "#000")
+                        .attr('x1', 0).attr('y1', 0);
+                    node.select('circle')
+                        .attr("r", 20).style('fill', '#4682b4');
+                    node.select('text.node-center')
+                        .attr("x", 0)
+                        .attr("dy", ".35em")
+                        .attr("text-anchor", "middle")
+                        .style('fill', '#fff')
+                        .text(function (d) {
+                            return d.metadata.name;
+                        });
+
+                    /*var _id = Date.now();
+                     node.select("foreignObject")
+                     .attr("class", "externalObject")
+                     .attr("width", 100)
+                     .attr("height", 100)
+                     .attr("x", 21)
+                     .attr("y", -11)
+                     .append("xhtml:div")
+                     .html("<input type='text' id='" + _id + "' placeholder='Add label' style='border: 1px solid #ccc;'>");
+
+                     // Remove nodes object with data
+                     node.exit().remove();
+
+                     var ele = $('#' + _id);
+                     ele.on("change", function () {
+                     node.select('text.node-label')
+                     .attr("x", 21)
+                     .attr("dy", ".35em")
+                     .attr("text-anchor", "start")
+                     .style('fill', '#000')
+                     .text($(this).val());
+                     node.select(".externalObject").remove();
+                     });*/
                 };
 
                 d3Infornite.prototype.visualization = function () {
@@ -161,13 +244,7 @@
                             .attr("r", function (d) {
                                 return (d.weight * 2) + 15;
                             });
-                        self.forceLayout.on('tick', function () {
-                            self.link.attr("d", self.svgDiagonal);
-
-                            self.node.attr('transform', function (d) {
-                                return 'translate(' + d.x + ', ' + d.y + ')';
-                            });
-                        });
+                        self.forceLayout.on('tick', fnTick);
                     }
                 };
 
@@ -210,21 +287,70 @@
                     self.redraw();
                 }
 
+                function fnTick() {
+                    self.link.attr("d", self.svgDiagonal);
+
+                    self.node.attr('transform', function (d) {
+                        return 'translate(' + d.x + ', ' + d.y + ')';
+                    });
+                }
+
                 function fnZoomed() {
                     self.chartContainer.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
                 }
 
-                function fnDragStarted() {
+                function fnDragStarted(d) {
                     d3.event.sourceEvent.stopPropagation();
-                    d3.select(this).classed('dragging', true);
+                    if (d3.select(this).attr('class') !== 'new-node' && self.chartType === 'force') {
+                        self.forceLayout.stop(); // stops the force auto positioning before you start dragging
+                    }
                 }
 
                 function fnDragged(d) {
-                    d3.select(this).attr('cx', d.x = d3.event.x).attr('cy', d.y = d3.event.y);
+                    if (d) {
+                        if (d3.select(this).attr('class') === 'new-node') {
+                            if (d3.select(this).attr('draw-line')) {
+                                if (d3.select(this).attr('id') !== d3.event.sourceEvent.toElement.parentNode.id) {
+                                    d.sourceEleId = d3.event.sourceEvent.toElement.parentNode.id;
+                                    d.targetEleId = d3.select(this).attr('id');
+                                }
+                                d3.select(this).select('line').attr('x2', (d3.event.x - d.x)).attr('y2', (d3.event.y - d.y));
+                            } else {
+                                d.x = d3.event.x;
+                                d.y = d3.event.y;
+                                d3.select(this).attr('transform', 'translate(' + d3.event.x + ', ' + d3.event.y + ')');
+                            }
+                        } else {
+                            var dx = d3.event.dx,
+                                dy = d3.event.dy;
+                            if (self.chartType === 'force') {
+                                d.px += dx;
+                                d.py += dy;
+                                d.x += dx;
+                                d.y += dy;
+                                fnTick();
+                            }
+                        }
+                    }
                 }
 
-                function fnDragEnded() {
-                    d3.select(this).classed('dragging', false);
+                function fnDragEnded(d) {
+                    d3.select(this).select('line').attr('x1', 0).attr('y1', 0).attr('x2', 0).attr('y2', 0);
+                    if (d.sourceEleId) {
+                        var edgeObj = {
+                            id: Date.now(), source: parseInt(d.sourceEleId), target: parseInt(d.targetEleId),
+                            directed: "true", "relation": "USES_CALC_COMPONENT"
+                        };
+                        self.originalData.edges.push(edgeObj);
+                        self.update(angular.copy(self.originalData));
+                        self.newNodesArr = [];
+                        self.newNodes.selectAll('.new-node').remove();
+                    }
+                    if (d3.select(this).attr('class') !== 'new-node' && self.chartType === 'force') {
+                        d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
+                        fnTick();
+                        self.forceLayout.resume();
+                    }
                 }
 
                 function fnRatio(width) {
@@ -254,19 +380,49 @@
                 templateUrl: 'common/directives/infornite/infornite.html',
                 link: function ($scope, iElement, iAttrs) {
 
-                    var chart = new d3Infornite({chartId: 'infornite'});
+                    var toggle = angular.element('#ss_toggle');
+                    var menu = angular.element('#ss_menu');
+                    var rot, chart;
+
+                    /*---- START: Add Node Button Click Menu -----*/
+                    toggle.on('click', function (ev) {
+                        rot = parseInt(angular.element(this).data('rot')) - 180;
+                        menu.css('transform', 'rotate(' + rot + 'deg)');
+                        menu.css('webkitTransform', 'rotate(' + rot + 'deg)');
+                        if ((rot / 180) % 2 === 0) {
+                            //Moving in
+                            toggle.parent().addClass('ss_active');
+                            toggle.children().removeClass('glyphicon-plus');
+                            toggle.children().addClass('glyphicon-remove');
+                        } else {
+                            //Moving Out
+                            toggle.parent().removeClass('ss_active');
+                            toggle.children().removeClass('glyphicon-remove');
+                            toggle.children().addClass('glyphicon-plus');
+                        }
+                        angular.element(this).data('rot', rot);
+                    });
+
+                    menu.on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+                        if ((rot / 180) % 2 === 0) {
+                            angular.element('#ss_menu div i').addClass('ss_animate');
+                        } else {
+                            angular.element('#ss_menu div i').removeClass('ss_animate');
+                        }
+                    });
+                    /*---- END: Add Node Button Click Menu -----*/
+
+                    chart = new d3Infornite({chartId: 'infornite'});
                     chart.update(angular.copy($scope.data));
 
-                    $scope.fnAddNewNode = function () {
-                        var newNodeObj = {
-                            id: Date.now(),
-                            label: ['entity', 'metric'],
-                            type: '',
-                            metadata: {},
-                            color: 'green'
+                    $scope.fnAddNewNode = function (text) {
+                        var newNodeData = {
+                            "id": Date.now(),
+                            "label": ["entity", "businessObject"],
+                            "type": "",
+                            "metadata": {"dateCreated": Date.now(), "name": text, "description": ""}
                         };
-                        $scope.data.nodes.push(newNodeObj);
-                        chart.update(angular.copy($scope.data));
+                        chart.addNewNode(newNodeData);
                     };
 
                     $scope.fnChangeChartView = function () {
